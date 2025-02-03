d372c109dbc9295a681e5bcd046c6187f8f291a9
Pranay K R <145147762+Pranay50x@users.noreply.github.com>
2025-02-03 13:03:05 +0530

    Update 4a.sh

--- Program-4/4a.sh
+++ Program-4/4a.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-space_usage=$(df -h / | awk 'NR==2 {print $5}' | sed 's/%//')
-echo "System Space usage: $space_usage%"
+space_usage=$(df -h / | awk 'NR==2 {print$5}' | sed 's/%//')
+echo "System space used: $space_usage%"
 
-if [ "$space_usage" -gt 80 ]; then 
-	
-	
-	echo "Low System Space"
-	find / -type f -size +1G -exec ls -lh {} \; 2>/dev/null 
+if [ $space_usage -gt 80 ];
+then
+    echo "Low system space left"
+    echo "Large files are: "
+    find / -type f -size +1G -exec ls -lh {} \; 2</dev/null
 
+    if [ $? -ne 0 ];
+    then
+        echo "no files greater than 1gb or insufficient perms"
+    fi
+else
+    echo "System space under control"
 fi
